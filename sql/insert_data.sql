cc6a9803aca6b32dbc1ecdcf898926cf60dcd559
onedrive228 <tdkoldyn@gmail.com>
2026-03-14 02:52:09 +0300

    feat: add 7 more car records for analysis

--- sql/insert_data.sql
+++ sql/insert_data.sql
@@ -1,4 +1,11 @@
 INSERT INTO cars (brand, model, year, color, price) VALUES
 ('Toyota', 'Camry', 2020, 'Silver', 1500000.00),
 ('BMW', 'X5', 2022, 'Black', 4500000.50),
-('Lada', 'Vesta', 2021, 'White', 800000.00);
+('Lada', 'Vesta', 2021, 'White', 800000.00),
+('Ford', 'Focus', 2019, 'Blue', 1100000.00),
+('Hyundai', 'Solaris', 2021, 'Gray', 950000.00),
+('Kia', 'Rio', 2022, 'Red', 1050000.00),
+('Mercedes', 'E-Class', 2023, 'Black', 5500000.00),
+('Audi', 'A4', 2020, 'White', 2800000.00),
+('Volkswagen', 'Polo', 2021, 'Silver', 1200000.00),
+('Nissan', 'Qashqai', 2022, 'Green', 2100000.00);
